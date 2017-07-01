a45f8a890e91ca68c8065d1b0663ab42677437bd
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2017-07-01 19:36:18 +0100

    Improve POIs but place names not appearing

--- resources/process-otm.lua
+++ resources/process-otm.lua
@@ -5,7 +5,7 @@
 
 -- Nodes will only be processed if one of these keys is present
 
-node_keys = { "amenity", "shop" }
+node_keys = { "amenity", "shop", "sport", "tourism", "place" }
 
 -- Initialize Lua logic
 
@@ -21,11 +21,53 @@ end
 function node_function(node)
 	local amenity = node:Find("amenity")
 	local shop = node:Find("shop")
-	if amenity~="" or shop~="" then
+	local sport = node:Find("sport")
+	local tourism = node:Find("tourism")
+
+	if amenity~="" or shop~="" or sport~="" then
 		node:Layer("poi", false)
-		if amenity~="" then node:Attribute("class",amenity)
-		else node:Attribute("class",shop) end
+		local rank = 10
+		if amenity~="" then 
+			rank = 3
+			node:Attribute("class",amenity)
+		elseif shop~="" then 
+			node:Attribute("class",shop)
+			rank = 4 
+		elseif sport~="" then 
+			node:Attribute("class",sport)
+			rank = 5
+		elseif tourism~="" then 
+			node:Attribute("class",tourism)
+			rank = 6
+		end
 		node:Attribute("name", node:Find("name"))
+		node:AttributeNumeric("rank", rank)
+	end
+
+	local place = node:Find("place")
+	if place ~= "" then
+		local rank = 1
+
+		if place == "continent" then rank = 1 end
+		if place == "country" then rank = 1 end
+		if place == "state" then rank = 1 end
+		if place == "city" then rank = 1 end
+		if place == "town" then rank = 1 end
+		if place == "village" then rank = 1 end
+		if place == "suburb" then rank = 1 end
+		if place == "neighbourhood" then rank = 1 end
+		if place == "locality" then rank = 1 end		
+		if place == "hamlet" then rank = 1 end
+		node:Layer("poi", false)
+		local name = node:Find("name")
+		node:Attribute("name", name)
+		name_en = name
+		if node:Find("name:en") ~= "" then
+			name_en = node:Find("name:en")
+		end
+		node:Attribute("name_en", name_en)
+		node:AttributeNumeric("rank", rank)
+		node:Attribute("class", place)
 	end
 end
 
