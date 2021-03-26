9f9ddab59d8ba83d008f83ceaeae5334676535af
Wouter van Kleunen <wouter.van@kleunen.nl>
2021-03-26 07:58:32 +0000

    Revert "Small optimization: store multipolygon in mmap in place"
    This reverts commit 973896f806b3dff7d5d930b56910a94041b1cc65.

--- include/osm_store.h
+++ include/osm_store.h
@@ -801,8 +801,27 @@ public:
 	handle_t store_multi_polygon(generated &store, Input const &src)
 	{
 		 perform_mmap_operation([&]() {
-			store.multi_polygon_store->emplace_back();
-			boost::geometry::assign(store.multi_polygon_store->back(), src);
+			 mmap::multi_polygon_t result(store.multi_polygon_store->get_allocator());
+			 result.reserve(src.size());
+
+			for(auto const &polygon: src) {
+				mmap::polygon_t::inners_type inners(result.get_allocator());
+				inners.resize(polygon.inners().size());
+
+				mmap::polygon_t::ring_type outer(result.get_allocator());
+
+				// Copy the outer ring
+				boost::geometry::assign(outer, polygon.outer());
+
+				// Store the inner rings
+				for(std::size_t i = 0; i < polygon.inners().size(); ++i) {
+					boost::geometry::assign(inners[i], polygon.inners()[i]);
+				} 
+			
+				result.emplace_back(outer, inners);
+			}
+
+			store.multi_polygon_store->push_back(result);
 		});
 
 		return mmap_file.get_handle_from_address(&store.multi_polygon_store->back());
