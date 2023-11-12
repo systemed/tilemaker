22e60b9ca4ea35e346ed73b6c3a6257ea978de32
Colin Dellow <cldellow@gmail.com>
2023-11-12 17:33:25 -0500

    lock-free reads for keys, vector for pairs
    This is the groundwork for implementing two future improvements:
    
    - hot/cold pairs: there is a bimodal distribution of attribute
      frequency. `landuse=wood`, `tunnel=0` are often duplicated.
      `name=Sneed's Seed & Feed` is not.
    
      In the future, we'll try to re-use the "hot" pairs to avoid
      paying the cost of an AttributePair for them.
    
    - "short vectors" - similar to the short string optimization,
      we should be able to pack up to 6 pairs (3 hot, 3 cold) in
      the overhead that a vector would otherwise use.
    
    As it stands, this commit increases memory usage. But we'll claw
    a lot of it back, and then some.

--- include/attribute_store.h
+++ include/attribute_store.h
@@ -9,10 +9,92 @@
 #include <vector>
 #include <unordered_map>
 #include <tsl/ordered_set.h>
+#include <random>
+#include <iostream>
+
+inline std::ostream& operator<<(std::ostream& os, const vector_tile::Tile_Value& value) {
+	if (value.has_string_value()) os << "[str]" << value.string_value();
+	if (value.has_bool_value()) os << "[bool]" << value.bool_value();
+	if (value.has_float_value()) os << "[float]" << value.float_value();
+	return os;
+}
+
 
 /* AttributeStore - global dictionary for attributes */
 
 typedef uint32_t AttributeIndex; // check this is enough
+
+// All members of this class are thread-safe.
+//
+// AttributeKeyStore maintains a pointer to the live version.
+// Lookup misses will result in a new version being published.
+class AttributeKeyStoreImmutable {
+public:
+	AttributeKeyStoreImmutable(std::map<const std::string, uint16_t> keys2index): keys2index(keys2index) {
+	}
+
+	uint16_t key2index(const std::string& key) {
+		auto rv = keys2index.find(key);
+
+		if (rv == keys2index.end())
+			// 0 acts as a sentinel to say that it's missing.
+			return 0;
+
+		return rv->second;
+	}
+
+	const std::map<const std::string, uint16_t> getKeys2IndexMap() { return keys2index; }
+
+private:
+	std::map<const std::string, uint16_t> keys2index;
+};
+
+class AttributeKeyStore {
+public:
+	// We jump through some hoops to have no locks for most readers,
+	// locking only if we need to add the value.
+	static uint16_t key2index(const std::string& key) {
+		auto index = immutable->key2index(key);
+
+		if (index != 0)
+			return index;
+
+		std::lock_guard<std::mutex> lock(keys2index_mutex);
+
+		// 0 is used as a sentinel, so ensure that the 0th element is just a dummy element.
+		if (keys.size() == 0)
+			keys.push_back("");
+
+		// Double-check that it's not there - maybe we were in a race
+		auto reallyMissing = immutable->key2index(key);
+		if (reallyMissing != 0)
+			return reallyMissing;
+
+		uint16_t newIndex = keys.size();
+
+		// This is very unlikely. We expect more like 50-100 keys.
+		if (newIndex >= 65535)
+			throw std::out_of_range("more than 65,536 unique keys");
+
+		std::map<const std::string, uint16_t> newMap(immutable->getKeys2IndexMap());
+		newMap[key] = newIndex;
+		keys.push_back(key);
+
+		immutable = std::make_unique<AttributeKeyStoreImmutable>(newMap);
+		return newIndex;
+	}
+
+	static const std::string& getKey(uint16_t index) {
+		return keys[index];
+	}
+
+private:
+	static std::mutex keys2index_mutex;
+	static std::unique_ptr<AttributeKeyStoreImmutable> immutable;
+	// NB: we use a deque, not a vector, because a deque never invalidates
+	// pointers to its members as long as you only push_back
+	static std::deque<std::string> keys;
+};
 
 // AttributePair is a key/value pair (with minzoom)
 struct AttributePair {
@@ -21,7 +103,7 @@ struct AttributePair {
 	char minzoom;
 
 	AttributePair(std::string const &key, vector_tile::Tile_Value const &value, char minzoom)
-		: keyIndex(key2index(key)), value(value), minzoom(minzoom)
+		: keyIndex(AttributeKeyStore::key2index(key)), value(value), minzoom(minzoom)
 	{ }
 
 	bool operator==(const AttributePair &other) const {
@@ -33,59 +115,74 @@ struct AttributePair {
 	}
 
 	const std::string& key() const {
-		// To be thread-safe, you must only read the actual string value after all
-		// attribute sets have been constructed, e.g. when writing output values.
-		//
-		// In general, prefer to use keyIndex for equality checking when building
-		// up the sets of AttributePairs.
-		return keys[keyIndex];
+		return AttributeKeyStore::getKey(keyIndex);
 	}
+
+	enum class Index { BOOL, FLOAT, STRING };
+	static Index type_index(vector_tile::Tile_Value const &v) {
+		if     (v.has_string_value()) return Index::STRING;
+		else if(v.has_float_value())  return Index::FLOAT;
+		else                          return Index::BOOL;
+	}
+
+	size_t hash() const {
+		std::size_t rv = minzoom;
+		boost::hash_combine(rv, keyIndex);
+		boost::hash_combine(rv, type_index(value));
+
+		if(value.has_string_value())
+			boost::hash_combine(rv, value.string_value());
+		else if(value.has_float_value())
+			boost::hash_combine(rv, value.float_value());
+		else
+			boost::hash_combine(rv, value.bool_value());
+
+		return rv;
+	}
+};
+
+
+// Pick SHARD_BITS such that PAIR_SHARDS is at least 2x your number of cores.
+// This reduces the odds of lock contention on inserting/retrieving the "cold" pairs.
+// For now, it's hardcoded, perhaps it should be a function of the numThreads argument.
+#define SHARD_BITS 7
+#define PAIR_SHARDS (1 << SHARD_BITS)
+
+class AttributePairStore {
+public:
+	static const AttributePair& getPair(uint32_t i) {
+		uint32_t shard = i >> (32 - SHARD_BITS);
+		uint32_t offset = i & (~(~0u << (32 - SHARD_BITS)));
+
+		std::lock_guard<std::mutex> lock(pairRefs_mutex[shard]);
+		return pairRefs[shard][offset];
+	};
+
+	static uint32_t addPair(const AttributePair& pair);
 
 private:
-	static std::map<std::string, uint16_t> keys2index;
-	static std::vector<std::string> keys;
-	static std::mutex rw_mutex;
+	// We refer to all attribute pairs by index. To avoid contention,
+	// we shard the deques containing the pairs. If there are two shards,
+	// shard 1 starts from 0, shard 2 starts from 2^31, and so on.
+	// TODO: optimize for "hot" pairs, which will be stored in the first 64K entries,
+	//       so that we can refer to them by a short.
+	static std::vector<std::deque<AttributePair>> pairRefs;
+	static std::vector<std::mutex> pairRefs_mutex;
 
-	// TODO: the set of unique keys is relatively stable, like ~50 or so.
-	//
-	// Forcing all readers to take a lock might be expensive. Currently,
-	// other locks dominate processing. Once PR #578 lands, revisit to see
-	// if this is a bottleneck.
-	//
-	// If it is, re-write to have lock-free reads: publish a pointer to
-	// an immutable map/vector for lookups.
-	//
-	// If a reader fails to find the key it wants, it could then grab a lock,
-	// clone the map/vector, check again for its entry, insert if missing,
-	// and swap the pointer to the immutable map/vector.
-	static uint16_t key2index(const std::string& key) {
-		std::lock_guard<std::mutex> lock(rw_mutex);
-
-		try {
-			return keys2index.at(key);
-		} catch (std::out_of_range &err) {
-			uint32_t newIndex = keys.size();
-			if (newIndex > 65535)
-				throw std::out_of_range("more than 65,536 unique keys");
-			keys2index[key] = newIndex;
-			keys.push_back(key);
-			return newIndex;
-		}
-	}
 };
 
 // AttributeSet is a set of AttributePairs
 // = the complete attributes for one object
 struct AttributeSet {
 	static bool compare(vector_tile::Tile_Value const &lhs, vector_tile::Tile_Value const &rhs) {
-		auto lhs_id = type_index(lhs);
-		auto rhs_id = type_index(lhs);
+		auto lhs_id = AttributePair::type_index(lhs);
+		auto rhs_id = AttributePair::type_index(lhs);
 		if(lhs_id < rhs_id) return true;
 		if(lhs_id > rhs_id) return false;
 		switch(lhs_id) {
-			case Index::BOOL:    return lhs.bool_value() < rhs.bool_value();
-			case Index::FLOAT:   return lhs.float_value() < rhs.float_value();
-			case Index::STRING:  return lhs.string_value() < rhs.string_value();
+			case AttributePair::Index::BOOL:    return lhs.bool_value() < rhs.bool_value();
+			case AttributePair::Index::FLOAT:   return lhs.float_value() < rhs.float_value();
+			case AttributePair::Index::STRING:  return lhs.string_value() < rhs.string_value();
 		}
 		throw std::runtime_error("Invalid type in attribute store");
 	}
@@ -98,47 +195,78 @@ struct AttributeSet {
         }
     }; 
 
-	enum class Index { BOOL, FLOAT, STRING };
-	static Index type_index(vector_tile::Tile_Value const &v) {
-		if     (v.has_string_value()) return Index::STRING;
-		else if(v.has_float_value())  return Index::FLOAT;
-		else                          return Index::BOOL;
-	}
-
 	struct hash_function {
+		// Calculating the hash value requires indirection and locks, so
+		// we use a memoized version calculated once the AttributeSet is finalized.
 		size_t operator()(const AttributeSet &attributes) const {
-			auto idx = attributes.values.size();
-			for(auto const &i: attributes.values) {
-				boost::hash_combine(idx, i.minzoom);
-				boost::hash_combine(idx, i.keyIndex);
-				boost::hash_combine(idx, type_index(i.value));
-
-				if(i.value.has_string_value())
-					boost::hash_combine(idx, i.value.string_value());
-				else if(i.value.has_float_value())
-					boost::hash_combine(idx, i.value.float_value());
-				else
-					boost::hash_combine(idx, i.value.bool_value());
-			}
-			return idx;
+			return attributes.hash_value;
 		}
 	};
 	bool operator==(const AttributeSet &other) const {
-		return values==other.values;
+		if (hash_value == 0 && values.size() != 0)
+			std::cout << "unexpected hash value of 0 for this, values.size()=" << values.size() << std::endl;
+		if (other.hash_value == 0 && other.values.size() != 0) {
+			std::cout << "unexpected hash value of 0 for other, other.values.size()=" << other.values.size() << std::endl;
+		}
+
+		if (hash_value != other.hash_value)
+			return false;
+
+		if (values.size() != other.values.size())
+			return false;
+
+		// Equivalent if, for every value in values, there is a value in other.values
+		// whose pair is the same.
+		// TODO: finalize_set ought to ensure that everyone's values are sorted such
+		//  that we can just do a pairwise comparison.
+
+		for (const auto& myValue: values) {
+			bool ok = false;
+			const auto& myPair = AttributePairStore::getPair(myValue);
+			for (const auto& theirValue: other.values) {
+				const auto& theirPair = AttributePairStore::getPair(theirValue);
+
+				if (myPair == theirPair) {
+					ok = true;
+					break;
+				}
+			}
+
+			if (!ok)
+				return false;
+		}
+
+		return true;
 	}
 
-	std::set<AttributePair, key_value_less> values;
+	void finalize_set();
+
+	std::vector<uint32_t> values;
 
 	void add(AttributePair const &kv);
 	void add(std::string const &key, vector_tile::Tile_Value const &v, char minzoom);
 
 	AttributeSet() { }
-	AttributeSet(const AttributeSet &a) { values = a.values; } // copy constructor, don't copy lock
+	AttributeSet(const AttributeSet &a) {
+		// TODO: can we just use the default copy constructor?
+		// This was needed to avoid copying the atomic<bool> which I am currently
+		// discarding.
+		hash_value = a.hash_value;
+		values = a.values;
+	}
 
 private:
-	std::atomic<bool> lock_ = { false };
-	void lock() { while(lock_.exchange(true, std::memory_order_acquire)); }
-	void unlock() { lock_.store(false, std::memory_order_release); }
+	// hash_value is memoized when AttributeStore tries to add it.
+	// Storing this costs us 4 bytes, but we make that back via shenanigans
+	// that require a fast hash_value computation.
+	size_t hash_value;
+
+// TODO: Chesterton's (memory?) fence... is this being used to impose
+//   memory read barriers?
+// Maybe it ought not be quietly discarded?
+//	std::atomic<bool> lock_ = { false };
+//	void lock() { while(lock_.exchange(true, std::memory_order_acquire)); }
+//	void unlock() { lock_.store(false, std::memory_order_release); }
 };
 
 // AttributeStore is the store for all AttributeSets
@@ -147,8 +275,8 @@ struct AttributeStore {
 	mutable std::mutex mutex;
 	int lookups=0;
 
-	AttributeIndex add(AttributeSet const &attributes);
-	const std::set<AttributePair, AttributeSet::key_value_less>& get(AttributeIndex index) const;
+	AttributeIndex add(AttributeSet &attributes);
+	std::set<AttributePair, AttributeSet::key_value_less> get(AttributeIndex index) const;
 	void reportSize() const;
 	void doneReading();
 	
--- src/attribute_store.cpp
+++ src/attribute_store.cpp
@@ -1,28 +1,115 @@
 #include "attribute_store.h"
 
-// AttributePair
-std::map<std::string, uint16_t> AttributePair::keys2index;
-std::vector<std::string> AttributePair::keys;
-std::mutex AttributePair::rw_mutex;
+// AttributeKeyStore
+std::deque<std::string> AttributeKeyStore::keys;
+std::mutex AttributeKeyStore::keys2index_mutex;
+std::unique_ptr<AttributeKeyStoreImmutable> AttributeKeyStore::immutable(
+	new AttributeKeyStoreImmutable(
+		std::map<const std::string, uint16_t>()
+	)
+);
+
+// AttributePairStore
+// TODO: can we use a hash instead of an RNG for assigning? For now, we have hot spots
+// until we handle "hot" attribute pairs (eg tunnel=0).
+thread_local std::random_device dev;
+thread_local std::mt19937 rng(dev());
+thread_local std::uniform_int_distribution<std::mt19937::result_type> nextShard(0, PAIR_SHARDS-1);
+
+std::vector<std::deque<AttributePair>> AttributePairStore::pairRefs(PAIR_SHARDS);
+std::vector<std::mutex> AttributePairStore::pairRefs_mutex(PAIR_SHARDS);
+
+uint32_t AttributePairStore::addPair(const AttributePair& pair) {
+	// TODO: is it worth using a hash? We could also randomly assign pairs
+	// to shards.
+	// The pair has a `keyIndex`, so subsequent runs of the program will
+	// likely have a different hash value for the pair anyway, so there's
+	// no reproduceability benefit.
+	//
+	// Oh, yeah, until we do hot/cold, random assignment is probably needed...
+//		uint32_t hashValue = pair.hash();
+//		size_t shard = hashValue >> (32 - SHARD_BITS);
+//	timespec start, end;
+//	clock_gettime(CLOCK_MONOTONIC, &start);
+
+	size_t shard = nextShard(rng);
+//	uint64_t shardy = 1e9 * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+//	size_t shard = shardy % PAIR_SHARDS;
+//	std::cout << "nextShard=" << shard << std::endl << std::flush;
+	std::lock_guard<std::mutex> lock(pairRefs_mutex[shard]);
+//		std::cout << "pairRefs_mutex[" << shard << "]=" << pairRefs_mutex[shard].native_handle() << std::endl;
+	uint32_t offset = pairRefs[shard].size();
+	pairRefs[shard].push_back(pair);
+	uint32_t rv = (shard << (32 - SHARD_BITS)) + offset;
+//		std::cout << "adding pair to shard=" << shard << ", offset=" << offset << ", index=" << rv;
+	return rv;
+};
 
 
 // AttributeSet
 
 void AttributeSet::add(AttributePair const &kv) {
-	lock();
-	values.insert(kv);
-	unlock();
+	// TODO: implement hot/cold strategy to re-use popular pairs
+	uint32_t index = AttributePairStore::addPair(kv);
+	values.push_back(index);
 }
 void AttributeSet::add(std::string const &key, vector_tile::Tile_Value const &v, char minzoom) {
 	AttributePair kv(key,v,minzoom);
-	lock();
-	values.insert(kv);
-	unlock();
+	add(kv);
+}
+
+bool sortFn(const AttributePair* a, const AttributePair* b) {
+	if (a->minzoom != b->minzoom)
+		return a->minzoom < b->minzoom;
+
+	if (a->keyIndex != b->keyIndex)
+		return a->keyIndex < b->keyIndex;
+
+	if (a->value.has_string_value()) return b->value.has_string_value() && a->value.string_value() < b->value.string_value();
+	if (a->value.has_bool_value()) return b->value.has_bool_value() && a->value.bool_value() < b->value.bool_value();
+	if (a->value.has_float_value()) return b->value.has_float_value() && a->value.float_value() < b->value.float_value();
+	throw std::runtime_error("Invalid type in AttributeSet");
+}
+
+void AttributeSet::finalize_set() {
+	// Memoize the hash value of this set so we don't repeatedly calculate it
+	// when inserting/querying sets.
+	//
+	// TODO: sort its attribute pairs so == tests can do a simple pairwise comparisons
+	auto idx = values.size();
+
+	std::vector<const AttributePair*> pairs;
+	for(auto const &j: values) {
+		// NB: getPair takes a lock
+		const auto& i = AttributePairStore::getPair(j);
+		pairs.push_back(&i);
+	}
+	
+	// Sort pairs in a stable way, so that we produce the same hash
+	std::sort (pairs.begin(), pairs.end(), sortFn);
+
+	for (auto const &i: pairs) {
+		boost::hash_combine(idx, i->minzoom);
+		boost::hash_combine(idx, i->keyIndex);
+		boost::hash_combine(idx, AttributePair::type_index(i->value));
+
+		if(i->value.has_string_value())
+			boost::hash_combine(idx, i->value.string_value());
+		else if(i->value.has_float_value())
+			boost::hash_combine(idx, i->value.float_value());
+		else
+			boost::hash_combine(idx, i->value.bool_value());
+	}
+	hash_value = idx;
 }
 
 // AttributeStore
 
-AttributeIndex AttributeStore::add(AttributeSet const &attributes) {
+AttributeIndex AttributeStore::add(AttributeSet &attributes) {
+	// TODO: there's probably a way to use C++ types to distinguish a finalized
+	// and non-finalized AttributeSet, which would make this safer.
+	attributes.finalize_set();
+
 	std::lock_guard<std::mutex> lock(mutex);
 	lookups++;
 
@@ -36,9 +123,18 @@ AttributeIndex AttributeStore::add(AttributeSet const &attributes) {
 	return idx;
 }
 
-const std::set<AttributePair, AttributeSet::key_value_less>& AttributeStore::get(AttributeIndex index) const {
+// TODO: consider implementing this as an iterator, or returning a cheaper
+//       container than a set (vector?)
+std::set<AttributePair, AttributeSet::key_value_less> AttributeStore::get(AttributeIndex index) const {
 	try {
-		return attribute_sets.nth(index).key().values;
+		const auto pairIds = attribute_sets.nth(index).key().values;
+
+		std::set<AttributePair, AttributeSet::key_value_less> rv;
+		for (const auto& id: pairIds) {
+			rv.insert(AttributePairStore::getPair(id));
+		}
+
+		return rv;
 	} catch (std::out_of_range &err) {
 		throw std::runtime_error("Failed to fetch attributes at index "+std::to_string(index)+" - size is "+std::to_string(attribute_sets.size()));
 	}
@@ -46,6 +142,40 @@ const std::set<AttributePair, AttributeSet::key_value_less>& AttributeStore::get
 
 void AttributeStore::reportSize() const {
 	std::cout << "Attributes: " << attribute_sets.size() << " sets from " << lookups << " objects" << std::endl;
+
+	// Print detailed histogram of frequencies of attributes.
+	if (false) {
+		std::map<uint32_t, uint32_t> tagCountDist;
+
+		size_t pairs = 0;
+		std::map<AttributePair, size_t, AttributeSet::key_value_less> uniques;
+		for (const auto attr_set: attribute_sets) {
+			pairs += attr_set.values.size();
+
+			try {
+				tagCountDist[attr_set.values.size()]++;
+			} catch (std::out_of_range &err) {
+				tagCountDist[attr_set.values.size()] = 1;
+			}
+
+			for (const auto attr: attr_set.values) {
+				try {
+					uniques[AttributePairStore::getPair(attr)]++;
+				} catch (std::out_of_range &err) {
+					uniques[AttributePairStore::getPair(attr)] = 1;
+				}
+			}
+		}
+		std::cout << "AttributePairs: " << pairs << ", unique: " << uniques.size() << std::endl;
+
+		for (const auto entry: tagCountDist) {
+			std::cout << "tagCountDist " << entry.first << " tags occurs " << entry.second << " times" << std::endl;
+		}
+
+		for (const auto entry: uniques) {
+			std::cout << "attrpair freq= " << entry.second << " key=" << entry.first.key() <<" value=" << entry.first.value << std::endl;
+		}
+	}
 }
 
 void AttributeStore::doneReading() {
