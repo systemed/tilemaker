0d951acc8a30ac09da240fe810f8faa5501e59b0
Colin Dellow <cldellow@gmail.com>
2023-12-09 08:48:01 -0500

    buffer sqlite writes if needed (#608)
    Fixes #596, I think.
    
    Time to process Antarctica drops from 9m5s to 3m51s

--- include/mbtiles.h
+++ include/mbtiles.h
@@ -7,6 +7,14 @@
 #include <vector>
 #include "external/sqlite_modern_cpp.h"
 
+struct PendingStatement {
+	int zoom;
+	int x;
+	int y;
+	std::string data;
+	bool isMerge;
+};
+
 /** \brief Write to MBTiles (sqlite) database
 *
 * (note that sqlite_modern_cpp.h is very slightly changed from the original, for blob support and an .init method)
@@ -16,6 +24,12 @@ class MBTiles {
 	std::vector<sqlite::database_binder> preparedStatements;
 	std::mutex m;
 	bool inTransaction;
+
+	std::shared_ptr<std::vector<PendingStatement>> pendingStatements1, pendingStatements2;
+	std::mutex pendingStatementsMutex;
+
+	void insertOrReplace(int zoom, int x, int y, const std::string& data, bool isMerge);
+	void flushPendingStatements();
 
 public:
 	MBTiles();
